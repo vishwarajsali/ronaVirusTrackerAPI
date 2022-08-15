1e154e1bf1e88a87b8097f5209c2c03caf94d81e
vishwaraj <sali.vishwaraj@gmail.com>
2022-08-15 17:59:33 -0400

    added support for swagger

--- src/main/java/net/vishwaraj/ronavirustracker/RonaVirusTrackerApplication.java
+++ src/main/java/net/vishwaraj/ronavirustracker/RonaVirusTrackerApplication.java
@@ -1,11 +1,14 @@
 package net.vishwaraj.ronavirustracker;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@OpenAPIDefinition(info = @Info(title = "Corona Tracker API", version = "1.0", description = "Corona Daily cases"))
 public class RonaVirusTrackerApplication {
 
 	public static void main(String[] args) {
